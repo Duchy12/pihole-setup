505148ff623edb4d6655ade5453bcaefe976e969
Duchy <danielbabcan@gmail.com>
2023-06-04 22:09:58 +0200

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # pihole-setup
+
+## Install
+```
+curl -sSL https://raw.githubusercontent.com/Duchy12/pihole-setup/master/install.sh | bash
+```
+
+## NOTE
+- Health status has been removed due to the fact that it was sometimes finnicky and wouldn't work properly. I will be looking into a better way to implement this in the future.
+- The install script will now automatically install the latest version of Pi-hole docker image.
---
+++ setup.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# https://github.com/pi-hole/docker-pi-hole/blob/master/README.md
+
+# Based on the original pi-hole setup
+timezone=$(timedatectl show --property=Timezone --value)
+
+PIHOLE_BASE="${PIHOLE_BASE:-$(pwd)}"
+[[ -d "$PIHOLE_BASE" ]] || mkdir -p "$PIHOLE_BASE" || { echo "Couldn't create storage directory: $PIHOLE_BASE"; exit 1; }
+
+# Note: FTLCONF_LOCAL_IPV4 should be replaced with your external ip.
+# Note: port 6969 is adjusted due to other services often running on port 80/8080
+docker run -d \
+    --name pihole \
+    -p 53:53/tcp -p 53:53/udp \
+    -p 6969:80 \
+    -e TZ="$timezone" \
+    -v "${PIHOLE_BASE}/etc-pihole:/etc/pihole" \
+    -v "${PIHOLE_BASE}/etc-dnsmasq.d:/etc/dnsmasq.d" \
+    --dns=1.1.1.1 \
+    --restart=unless-stopped \
+    --hostname pi.hole \
+    -e VIRTUAL_HOST="pi.hole" \
+    -e PROXY_LOCATION="pi.hole" \
+    -e FTLCONF_LOCAL_IPV4="10.0.1.38" \
+    pihole/pihole:latest
+
+local_ip=$(hostname -I | grep -oP '^\S+')
+printf 'Starting up pihole container\n'
+printf 'Enter the password you want to use for pi-hole: '
+read password
+container_id=$(docker ps -q -n 1)
+docker exec -it $container_id $password -a -p $password -a -p
+echo -e "\n password: $password for pi-hole running at: http://$local_ip:6969/admin"
+exit 0
+done;
